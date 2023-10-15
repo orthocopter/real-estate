168c2685557450b798171f7b557d8a858d835fcf
CJ <cjvonex@gmail.com>
2023-10-15 00:34:19 -0400

    restoring main

---
+++ main.py
@@ -0,0 +1,10 @@
+import soup_redfin
+
+listing = soup_redfin.url_to_listing(url)
+
+# def calculate_monthly_cashflow (incomes, expenses) :
+
+# monthly_incomes = {}
+# monthly_expenses = {}
+
+print('DONE')
